4a3a8b9605b6323255d54a21708a48b4994fdad1
Hiromi <hiromif630@gmail.com>
2015-06-11 18:47:16 -0700

    Added comments to PostWantedAdActivity

--- app/src/main/java/edu/cascadia/bookmarked/BuySearchBookActivity.java
+++ app/src/main/java/edu/cascadia/bookmarked/BuySearchBookActivity.java
@@ -5,22 +5,15 @@ package edu.cascadia.bookmarked;
  */
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 
 public class BuySearchBookActivity extends Activity{
-
-   // private Button adButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +31,7 @@ public class BuySearchBookActivity extends Activity{
 
     // Method for Post a Wanted Ad button
     private void initPostWantedAdButton(){
-        Button adButton = (Button) findViewById(R.id.buttonPostAdSearch);
+        Button adButton = (Button) findViewById(R.id.buttonPostAd);
         adButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent i = new Intent(BuySearchBookActivity.this, PostWantedAdActivity.class);
@@ -48,26 +41,4 @@ public class BuySearchBookActivity extends Activity{
             }
         });
     }
-
-   /* public void addListenerOnButtonTextbookList() {
-
-
-        ImageView btnList1 = (ImageView)findViewById(R.id.imageButtonBookWanted1);
-
-        btnList1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intentbooklist1 = new Intent(CollegeSearch.this, TextbookListActivity.class);
-                startActivity(intentbooklist);
-
-            }
-        });
-    }
-*/
-
-
-
-
-
 }
