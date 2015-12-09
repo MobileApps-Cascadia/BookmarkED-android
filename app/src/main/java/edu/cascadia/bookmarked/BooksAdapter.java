ef3dffcf0381585e815e4f82fa40dee3e7421c32
Sean Chung <sean.chung8@gmail.com>
2015-12-08 16:12:21 -0800

    Modified the layout for the book wanted list item. It does not have the image view. Also deleted the unused book_list_item_row2.xml. Also renamed the book_list_item_row3.xml to book_list_item_with_image.xml to be more meaningful.

--- app/src/main/java/edu/cascadia/bookmarked/BooksAdapter.java
+++ app/src/main/java/edu/cascadia/bookmarked/BooksAdapter.java
@@ -12,15 +12,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by rattanak on 11/17/2015.
  */
 public class BooksAdapter extends ArrayAdapter<BookItem> {
+
+    private boolean forSaleListItem = false;
+
     public BooksAdapter(Context context, ArrayList<BookItem> booksList) {
         super(context, 0, booksList);
         //super(context, 0, booksList);
+    }
+
+    public void setForSaleListItem(boolean forSaleListItem) {
+        this.forSaleListItem = forSaleListItem;
     }
 
     @Override
@@ -29,33 +35,42 @@ public class BooksAdapter extends ArrayAdapter<BookItem> {
         BookItem bookItem = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.book_list_item_row3, parent, false);
+            if (forSaleListItem) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.book_list_item_with_image, parent, false);
+            } else {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.book_list_item, parent, false);
+            }
         }
         // Lookup view for data population
         TextView bookTitle = (TextView) convertView.findViewById(R.id.bookTitleTextView);
-        TextView bookPrice = (TextView) convertView.findViewById(R.id.bookPriceTextView);
-        ImageView bookImageView = (ImageView) convertView.findViewById(R.id.bookCoverimageView);
         // Populate the data into the template view using the data object
         bookTitle.setText(bookItem.title);
         TextView bookAuthor = (TextView) convertView.findViewById(R.id.bookAuthorTextView);
         bookAuthor.setText(bookItem.author);
 
-        if (Utility.isNotNull(bookItem.askingPrice)) {
-            bookPrice.setText(bookItem.askingPrice);
-        }
-        if (bookItem.base64Picture != null && bookItem.base64Picture.trim().length() > 0) {
-            try {
-                byte[] decodedString = Base64.decode(bookItem.base64Picture, Base64.DEFAULT);
-                Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        if (forSaleListItem) {
+            TextView bookPrice = (TextView) convertView.findViewById(R.id.bookPriceTextView);
+            ImageView bookImageView = (ImageView) convertView.findViewById(R.id.bookCoverimageView);
 
-                System.out.println("Bitmap width:" + bitmap.getWidth() + " height:" + bitmap.getHeight());
-
-                bookImageView.setImageBitmap(bitmap);
-            } catch (Exception e) {
-                System.out.println("Exception in getting image. " + e.getMessage());
+            if (Utility.isNotNull(bookItem.askingPrice)) {
+                bookPrice.setText(bookItem.askingPrice);
             }
-        }
+            if (bookItem.base64Picture != null && bookItem.base64Picture.trim().length() > 0) {
+                try {
+                    byte[] decodedString = Base64.decode(bookItem.base64Picture, Base64.DEFAULT);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
+                    System.out.println("Bitmap width:" + bitmap.getWidth() + " height:" + bitmap.getHeight());
+
+                    bookImageView.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    System.out.println("Exception in getting image. " + e.getMessage());
+                }
+            }
+        } else {
+            TextView isbnTextView = (TextView) convertView.findViewById(R.id.isbnTextView);
+            isbnTextView.setText(bookItem.isbn);
+        }
         //tvHome.setText(user.hometown);
         // Return the completed view to render on screen
         return convertView;
